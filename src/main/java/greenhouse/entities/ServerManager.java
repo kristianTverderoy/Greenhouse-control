3901e90c57364d2bda9f9dc825a3b5fe6565277d
Kristian <kristianty80@gmail.com>
2025-10-20 15:31:59 +0200

    Turned ServerManager into a singleton.

--- src/main/java/greenhouse/entities/ServerManager.java
+++ src/main/java/greenhouse/entities/ServerManager.java
@@ -4,6 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerManager {
+    private static ServerManager instance = null;
     private final Map<String, tcpServer> servers = new HashMap<>(); //Holds all servers by name.
 
+
+    private ServerManager(){
+
+    }
+
+    public void addServer(String name, tcpServer server) {
+        servers.put(name, server);
+    }
+
+    public tcpServer getServer(String name) {
+        return servers.get(name);
+    }
+
+    public static synchronized ServerManager getInstance(){
+        if (instance == null){
+            instance = new ServerManager();
+        }
+        return instance;
+    }
 }
