ddfc1d31122d9101b5a474b609f30ea1eb89034d
Kristian <kristianty80@gmail.com>
2025-10-28 14:44:21 +0100

    Fixed checkstyle.

---
+++ src/main/java/greenhouse/entities/entrypoints/ClientApp.java
@@ -0,0 +1,15 @@
+package greenhouse.entities.entrypoints;
+
+/**
+ * The program's entrypoint where the purpose is to start a clientside communication with a server.
+ */
+public class ClientApp {
+  /**
+   * The ClientApp's main entry method.
+   *
+   * @param args The commandline arguments to pass upon starting the program.
+   */
+  public static void main(String[] args) {
+
+  }
+}
--- src/main/java/greenhouse/entities/entrypoints/clientApp.java
+++
@@ -1,7 +0,0 @@
-package greenhouse.entities.entrypoints;
-
-public class clientApp {
-    public static void main(String[] args) {
-
-    }
-}
