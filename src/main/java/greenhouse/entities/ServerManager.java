29d586a732ee72415f379819586404c6ca26d702
Kristian <kristianty80@gmail.com>
2025-10-28 14:46:45 +0100

    Changed TCPServer to use proper name convention. Added some field variables in the TCPServer class.

--- src/main/java/greenhouse/entities/ServerManager.java
+++ src/main/java/greenhouse/entities/ServerManager.java
@@ -5,18 +5,18 @@ import java.util.Map;
 
 public class ServerManager {
     private static ServerManager instance = null;
-    private final Map<String, tcpServer> servers = new HashMap<>(); //Holds all servers by name.
+    private final Map<String, TCPServer> servers = new HashMap<>(); //Holds all servers by name.
 
 
     private ServerManager(){
 
     }
 
-    public void addServer(String name, tcpServer server) {
+    public void addServer(String name, TCPServer server) {
         servers.put(name, server);
     }
 
-    public tcpServer getServer(String name) {
+    public TCPServer getServer(String name) {
         return servers.get(name);
     }
 
--- src/main/java/greenhouse/entities/TCPServer.java
+++ src/main/java/greenhouse/entities/TCPServer.java
@@ -2,10 +2,17 @@ package greenhouse.entities;
 
 
 import java.net.ServerSocket;
+import java.util.List;
 
-public class tcpServer {
+public class TCPServer {
+    private ServerSocket mainServerSocket;
+    private ServerSocket listenerServerSocket;
+    private int port;
+    private List<ServerSubscriber> serverSubscribers;
 
-    public tcpServer(){
+
+
+    public TCPServer(){
 
     }
 
