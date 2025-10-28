77c86b42c164f41355243505a08cff36bd2a29ea
Kristian <kristianty80@gmail.com>
2025-10-28 14:35:12 +0100

    Fixed checkstyle.

--- src/main/java/greenhouse/entities/ServerSubscriber.java
+++ src/main/java/greenhouse/entities/ServerSubscriber.java
@@ -1,16 +1,15 @@
 package greenhouse.entities;
 
 /**
- * An interface defining a set of methods all that any object that would like to subscribe to changes in objects of the
- * ServerSubscriber class, must implement.
+ * An interface defining a set of methods all that any object that would like to subscribe
+ * to changes in objects of the ServerSubscriber class, must implement.
  *
- * An object of the TempSensor class will then be able to treat objects of any class as a ServerSubscriber as long as
- * the class of the object has implemented this interface.
+ * <p>An object of the TempSensor class will then be able to treat objects of any class as a
+ * ServerSubscriber as long as the class of the object has implemented this interface.
  */
 public interface ServerSubscriber {
-
-    /**
-     * Called whenever the observed object need to signal its observers that a change has occurred.
-     */
-    void update();
+  /**
+   * Called whenever the observed object need to signal its observers that a change has occurred.
+   */
+  void update();
 }
