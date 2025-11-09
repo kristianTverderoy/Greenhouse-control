415d665ae46ad5d927ea5d14e22258a07aea70de
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2025-11-09 20:47:03 +0100

    Made test method name more descriptive, and made it work with air's methods on the sensors branch

--- src/test/java/greenhouse/entities/AirTest.java
+++ src/test/java/greenhouse/entities/AirTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AirTest {
 
   @Test
-  public void stateChangeUpdate() {
+  public void updateStateShouldChangeHumidityLuxAndTemperatureEachUpdate() {
     Air airInGreenHouse = new Air(20, 0.5f);
     int humidityChanges = 0;
     int lightChanges = 0;
@@ -17,20 +17,19 @@ public class AirTest {
 
     for (int i = 0; i < 10; i++) {
       float preUpdateHumidity = airInGreenHouse.getHumidity();
-      double preUpdateLight = airInGreenHouse.getLight();
+      double preUpdateLight = airInGreenHouse.getLux();
       double preUpdateTemp = airInGreenHouse.getTemperature();
       airInGreenHouse.updateState();
-      System.out.println("Humidity: " + airInGreenHouse.getHumidity() + ", Light: " + airInGreenHouse.getLight() +
-              ", Temp: " + airInGreenHouse.getTemperature());
 
       if (preUpdateHumidity != airInGreenHouse.getHumidity()) humidityChanges++;
-      if (preUpdateLight != airInGreenHouse.getLight()) lightChanges++;
+      if (preUpdateLight != airInGreenHouse.getLux()) lightChanges++;
       if (preUpdateTemp != airInGreenHouse.getTemperature()) tempChanges++;
+      airInGreenHouse.tick();
     }
 
-    assertTrue(humidityChanges >= 8, "Humidity changed " + humidityChanges + "/10 times");
-    assertTrue(lightChanges >= 8, "Light changed " + lightChanges + "/10 times");
-    assertTrue(tempChanges >= 8, "Temperature changed " + tempChanges + "/10 times");
+    assertTrue(humidityChanges >= 10,  "Humidity changed " + humidityChanges + "/10 times");
+    assertTrue(lightChanges >= 10, "Light changed " + lightChanges + "/10 times");
+    assertTrue(tempChanges >= 10, "Temperature changed " + tempChanges + "/10 times");
   }
 }
 
