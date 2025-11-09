85c0cb2dab99dc72b8b63085ad59ace15cf72fcf
Kristian <kristianty80@gmail.com>
2025-11-09 14:15:56 +0100

    Added temperature simulation logic.

--- src/main/java/greenhouse/entities/Air.java
+++ src/main/java/greenhouse/entities/Air.java
@@ -1,7 +1,5 @@
 package greenhouse.entities;
 
-import java.util.Random;
-
 public class Air implements Sensorable {
 
   private float humidity; // 0 <= x <= 1
@@ -29,7 +27,7 @@ public class Air implements Sensorable {
       humidity = (float) (Math.random());
       initialHumiditySet = true;
     }
-    // light (lux): 0.0 - 1000.0
+    // light (lux): 0.0 - 100000.0
     if (light == 0.0 && !initialLightSet) {
       light = (double) (Math.random() * 100000);
       initialLightSet = true;
@@ -40,14 +38,14 @@ public class Air implements Sensorable {
       initialTempSet = true;
     }
 
-    if (temperature < tempTarget || temperature > tempTarget) {
-      handleTemperature();
-    } else if (temperature == tempTarget) {
-      changeTemperature(0.5f);
-    }
+    tempUpProbability();
   }
 
-  private void handleTemperature() {
+  /**
+   * Simulates temperature change probability based on the difference
+   * between current temperature and target temperature.
+   */
+  private void tempUpProbability() {
     float tempIncreaseChance;
     if (Math.abs(temperature - tempTarget) > 20) {
       tempIncreaseChance = 0.9f;
@@ -66,19 +64,30 @@ public class Air implements Sensorable {
     }
   }
 
-  private void changeTemperature(float increaseChance){
-    //TODO: implement
+  /**
+   * Changes the temperature up or down based on the given chance to increase.
+   * If the temperature is lower than the target temperature, the chance to increase is higher,
+   * while if it's higher than the target temperature, the chance to decrease is higher.
+   *
+   * @param increaseChance The chance to increase the temperature.
+   */
+  private void changeTemperature(float increaseChance) {
+    float temperatureChange = (float) (Math.random() * 2); // 0-2°C
+    if (Math.random() < increaseChance) {
+      temperature += temperatureChange;
+    } else temperature -= temperatureChange;
   }
 
-  public float getHumidity() {
-    return humidity;
-  }
 
-  public double getLight() {
-    return light;
-  }
+public float getHumidity() {
+  return humidity;
+}
 
-  public double getTemperature() {
-    return temperature;
-  }
+public double getLight() {
+  return light;
+}
+
+public double getTemperature() {
+  return temperature;
+}
 }
