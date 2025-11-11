85ba2f011c651714fc213c732acd7a5bc036e377
Jogeir <jjliavaa@stud.ntnu.no>
2025-11-11 16:09:20 +0100

    add actuators to change environment in greenhouse

---
+++ src/main/java/greenhouse/entities/actuators/Actuator.java
@@ -0,0 +1,46 @@
+package greenhouse.entities.actuators;
+
+/**
+ * Abstract class for all actuator types in the greenhouse system.
+ * This class provides common functionality for managing power state and id.
+ */
+public abstract class Actuator {
+  private String type;
+  private int id;
+  private boolean isOn = false;
+
+  /**
+   * Constructs a new Actuator with the specified parameters.
+   * 
+   * @param type the type of actuator (e.g., "humidifier", "lamp", "sprinkler")
+   * @param id the unique identifier for this actuator
+   * 
+   */
+  public Actuator(String type, int id) {
+    this.type = type;
+    setId(id);
+  }
+  
+  /**
+   * Sets the id of the actuator
+   * @param id the id of the actuator
+   */
+  private void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * Changes isOn to false if it was true and vice versa.
+   */
+  public void togglePower() {
+    this.isOn = !this.isOn;
+  }
+
+  /**
+   * Returns the true or false if the actuator is on.
+   * @return the value of isOn
+   */
+  public boolean getPowerState() {
+    return this.isOn;
+  }
+}
---
+++ src/main/java/greenhouse/entities/actuators/Sprinkler.java
@@ -0,0 +1,20 @@
+package greenhouse.entities.actuators;
+
+/**
+ * Actuator responsible for watering plants in the greenhouse.
+ * The sprinkler controls water distribution to maintain optimal moisture levels
+ * for plant growth.
+ */
+public class Sprinkler extends Actuator {
+
+  /**
+   * Constructs a new Sprinkler with the specified parameters.
+   *
+   * @param type the type identifier for this actuator
+   * @param id the unique identifier for this sprinkler
+   */
+  public Sprinkler(String type, int id) {
+    super(type, id);
+  }
+
+}
