1a57f166bd879811dc88f26e2ffca70a98e8cb1f
Jogeir <jjliavaa@stud.ntnu.no>
2025-11-14 15:50:24 +0100

    add unfinished actuator for raising nitrogen level in soil

---
+++ src/main/java/greenhouse/entities/actuators/Fertilizer.java
@@ -0,0 +1,11 @@
+package greenhouse.entities.actuators;
+
+public class Fertilizer extends Actuator {
+
+  //TODO: finish class (Frida) also requires a nitrogen level sensor
+  // Raises nitrogen level
+  public Fertilizer(String type, int id) {
+    super(type, id);
+  }
+  
+}
