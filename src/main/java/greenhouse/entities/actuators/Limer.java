6776642f8e7f0a3262d0a4eeca7b6f8e241e85ad
Jogeir <jjliavaa@stud.ntnu.no>
2025-11-14 15:51:17 +0100

    add unfinished class to raise PH level in soil

---
+++ src/main/java/greenhouse/entities/actuators/Limer.java
@@ -0,0 +1,11 @@
+package greenhouse.entities.actuators;
+
+public class Limer extends Actuator {
+
+  //TODO: Finish class (Frida)
+  // Changes PH value
+  public Limer(String type, int id) {
+    super(type, id);
+  }
+  
+}
