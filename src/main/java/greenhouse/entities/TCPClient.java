b9cd174d8939f5a34f746162f9d238371cab14ed
Jogeir <jjliavaa@stud.ntnu.no>
2025-11-19 13:00:44 +0100

    Remove unused code

--- src/main/java/greenhouse/entities/TCPClient.java
+++ src/main/java/greenhouse/entities/TCPClient.java
@@ -109,52 +109,6 @@ public class TCPClient {
       return listenerThread;
   }
 
-//  //TODO: Remove sout statements when no longer necessary for debugging.
-//  public void connectToServer(String host, int port){
-//    try (Socket socket = new Socket(host, port);
-//         InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-//         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-//         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-//         BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-//         Scanner scanner = new Scanner(System.in)) {
-//
-//      this.bufferedReader = bufferedReader;
-//      this.bufferedWriter = bufferedWriter;
-//
-//      // Thread to listen for server messages:
-//      Thread listenerThread = new Thread(() -> {
-//        try {
-//          String serverMessage;
-//          while ((serverMessage = bufferedReader.readLine()) != null) {
-//            System.out.println(decryptMessage(serverMessage));
-//          }
-//        } catch (IOException e) {
-//          System.err.println("Connection to server lost: " + e.getMessage());
-//          }
-//        });
-//      listenerThread.setDaemon(true);
-//      listenerThread.start();
-//
-//      // Main loop only sends messages to server.
-//      boolean disconnected = false;
-//      while (!disconnected) {
-//
-//        String messageToSend = scanner.nextLine();
-//        String encryptedMessage = encryptMessage(messageToSend);
-//
-//        bufferedWriter.write(encryptedMessage);
-//        bufferedWriter.newLine();
-//        bufferedWriter.flush();
-//
-//        if (messageToSend.equalsIgnoreCase("exit")){
-//          disconnected = true;
-//        }
-//      }
-//    } catch (Exception e){
-//      System.err.println("Could not connect to server: " + e.getMessage());
-//    }
-//  }
-
   /**
    * Attempts to reconnect to the server with exponential backoff.
    * @param host server hostname or IP
