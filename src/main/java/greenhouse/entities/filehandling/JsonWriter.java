1224008b76411bdc402df1ca7e9f9e47fd1a87a5
Kristian <kristianty80@gmail.com>
2025-11-19 18:33:24 +0100

    Add nextSensorId and nextApplianceId handling in GreenHouse creation

--- src/main/java/greenhouse/entities/filehandling/JsonWriter.java
+++ src/main/java/greenhouse/entities/filehandling/JsonWriter.java
@@ -85,6 +85,10 @@ public class JsonWriter {
     // Include air and soil state
     dto.setAir(convertAirToDTO(greenhouse.getAir()));
     dto.setSoil(convertSoilToDTO(greenhouse.getSoil()));
+    dto.setNextSensorId(greenhouse.getNextSensorId());
+    dto.setNextApplianceId(greenhouse.getNextApplianceId());
+
+
     return dto;
   }
 
